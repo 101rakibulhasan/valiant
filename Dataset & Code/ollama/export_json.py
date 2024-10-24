d8cc686fed0e35583648ebe9356edde33c34b332
101rakibulhasan <101rakibulhasan@gmail.com>
2024-10-25 00:16:55 +0600

    directory changed

--- Dataset & Code/ollama/export_json.py
+++
@@ -1,9 +0,0 @@
-from bson.json_util import dumps
-from pymongo import MongoClient
-import json
-from judge_mongodb import collection
-
-if __name__ == '__main__':
-    cursor = collection.find({})
-    with open('collection.json', 'w') as file:
-        json.dump(json.loads(dumps(cursor)), file, indent=4)
